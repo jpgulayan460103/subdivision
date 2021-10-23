248432610db8d7d20aa4db56c6e7301b4d5fac87
John Paul <jpgulayan@gmail.com>
2021-10-23 16:21:16 +0800

    change labels

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>RFID Subdivision</title>
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<style>
 		.form-error{
@@ -23,7 +23,7 @@
 		<div class="modal-dialog">
 			<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title">Modal title</h5>
+				<h5 class="modal-title">Scan RFID</h5>
 				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 			</div>
 			<div class="modal-body">
